3cd9c082660a2c4b02b8a049c1d864b2869c4845
MaheshAravindV <mahesharavind@student.tce.edu>
2021-12-17 13:04:35 +0530

    Added 3 more forms

--- index.js
+++ index.js
@@ -6,13 +6,18 @@ const app = express();
 app.use(cors());
 app.use(express.static("frontend"));
 app.use(express.json());
-if (false /* Make true if in dev */) process.env.PORT = 3000;
+if (true /* Make true if in dev */) process.env.PORT = 3000;
 app.listen(process.env.PORT);
 
 app.post("", (req, res) => {
-  fs.appendFileSync("data.csv", req.body.row.toString() + "\n", (err) => {
-    if (err) res.send(err).status(400);
-  });
+  console.log(req.body);
+  fs.appendFileSync(
+    req.body.formtype + ".csv",
+    req.body.row.toString() + "\n",
+    (err) => {
+      if (err) res.send(err).status(400);
+    }
+  );
   res.status(200).send("Success Entry added");
 });
 
